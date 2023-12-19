de6b746b4b14d81722fec18c7bbd19270cac06c0
Maciej Szymański <30301744+Macszym@users.noreply.github.com>
2023-12-19 17:27:56 +0100

    Update Hopptok.py

--- Hopptok.py
+++ Hopptok.py
@@ -2,8 +2,39 @@
 import sys
 import random 
 import pygame 
-from pygame.locals import *
   
 # Deklaracja zmiennych okna 
-window_width = 1000
-window_height = 300
+window_width = 800
+window_height = 500
+
+import pygame
+import sys
+
+# Inicjalizacja Pygame
+pygame.init()
+
+# Ustawienia ekranu
+screen = pygame.display.set_mode((window_width, window_height))
+
+# Ustawienia ptaka
+bird = pygame.Rect(35, 35, 35, 35)
+
+# Ustawienia grawitacji
+gravity = 0.25
+bird_movement = 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 6
+
+    screen.fill((255, 255, 255))
+    bird_movement += gravity
+    bird.centery += bird_movement
+    pygame.draw.rect(screen, (255, 0, 0), bird)
+    pygame.display.update()
